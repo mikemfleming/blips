9f868acc00a3583a8c18fc1bd70a5f7a9b9b72ae
miek <flemingatx@gmail.com>
2018-04-01 11:47:10 -0500

    gets current live cells

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,13 +5,19 @@ import { PERIOD_MS } from '../../config/main.config';
 import Grid from '../components/Grid';
 import { STEP_GRID, START_GAME, STOP_GAME } from '../constants';
 
-const App = ({ grid, stepGrid, currentColumn, start, stop }) => (
-  <div>
-    <button onClick={start}>Start</button>
-    <button onClick={stop}>Stop</button>
-    <Grid grid={grid} currentColumn={currentColumn} />
-  </div>
-);
+const App = ({ grid, stepGrid, currentColumn, start, stop }) => {
+  const currentCells = currentColumn > -1
+    ? grid.map(row => row[currentColumn]).filter(cell => cell.status)
+    : null;
+  
+  return (
+    <div>
+      <button onClick={start}>Start</button>
+      <button onClick={stop}>Stop</button>
+      <Grid grid={grid} currentColumn={currentColumn} />
+    </div>
+  );
+};
 
 const mapStateToProps = state => ({
   grid: state.grid,

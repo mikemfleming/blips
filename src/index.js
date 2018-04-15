5a87dbb0ed8d49c698baf7aee49be1f4aad31880
miek <flemingatx@gmail.com>
2018-04-15 16:06:34 -0500

    adds react router for about section

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
+import styled from 'styled-components';
+import { BrowserRouter, Route, BrowserHistory } from 'react-router-dom';
 
 import App from './containers/App';
 import reducer from './reducers';
@@ -10,9 +12,25 @@ import reducer from './reducers';
 const middleware = applyMiddleware(createLogger());
 const store = createStore(reducer, middleware);
 
+const AppContainer = styled.div`
+  margin: auto;
+  max-width: 40vw;
+  font-size: 1.5rem;
+  @media (max-width: 768px) {
+    max-width: 90vw;
+  }
+`;
+
+const About = () => <div>asdasda</div>
+
 ReactDOM.render(
 	<Provider store={store} >
-    <App />
+    <BrowserRouter history={BrowserHistory}>
+      <AppContainer>
+        <Route path="/" component={App}/>
+        <Route path="/about" component={About}/>
+      </AppContainer>
+    </BrowserRouter>
   </Provider>,
 	document.getElementById('root')
 );

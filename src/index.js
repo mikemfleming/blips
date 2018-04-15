b5df8f44d069959b6e1cd183fc04d4a69a1b915e
miek <flemingatx@gmail.com>
2018-04-15 18:27:46 -0500

    adds placeholder text

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
 import styled from 'styled-components';
-import { BrowserRouter, Route, BrowserHistory } from 'react-router-dom';
+import { BrowserRouter, Route, BrowserHistory, Link } from 'react-router-dom';
 
 import App from './containers/App';
 import reducer from './reducers';
@@ -21,13 +21,30 @@ const AppContainer = styled.div`
   }
 `;
 
-const About = () => <div>asdasda</div>
+const GridThing = styled.div`
+  height: 40vw;
+  @media (max-width: 768px) {
+    height: 90vw;
+  }
+`;
+
+const About = () => (
+  <div>
+    <Link to="/" className="link dim black"><h1 className="mb2">Blips of Life</h1></Link>
+    <GridThing className="pl3 pr3 ba bw3">
+      <div className="f5 lh-copy measure tj center">
+        <p>Lorem ipsum dolor amet coloring book gochujang tote bag, vice enamel pin prism whatever pitchfork. Snackwave tbh neutra poutine, hell of bespoke next level drinking vinegar shaman. Church-key chartreuse chambray, biodiesel live-edge jianbing wayfarers disrupt umami cold-pressed.</p>
+        <p>Dreamcatcher irony woke shabby chic. Fingerstache cronut chia shaman, normcore man bun cray VHS tbh iceland poutine bespoke. Shabby chic bushwick ennui pabst, skateboard 3 wolf moon vexillologist. Mixtape pitchfork pour-over twee austin, wayfarers you probably haven't heard of them vaporware tumeric.</p>
+      </div>
+    </GridThing>
+  </div>
+);
 
 ReactDOM.render(
 	<Provider store={store} >
     <BrowserRouter history={BrowserHistory}>
       <AppContainer>
-        <Route path="/" component={App}/>
+        <Route path="/" exact component={App}/>
         <Route path="/about" component={About}/>
       </AppContainer>
     </BrowserRouter>

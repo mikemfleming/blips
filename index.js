02c4659eae31443be33bc4b034fee79149f09577
miek <flemingatx@gmail.com>
2018-04-21 08:37:58 -0500

    adds favicon

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const express = require('express');
 const path = require('path');
 const log = require('./logger');
 const pino = require('pino-http')({ logger: log });
+const favicon = require('serve-favicon');
 
 const { PORT } = require('./config/main.config');
 
 const app = express();
+app.use(favicon(path.join((__dirname, 'favicon.png'))));
 app.use(pino);
 
 app.get('*/script.js', (req, res) => res.sendFile(path.join(__dirname, 'dist/bundle.js')));

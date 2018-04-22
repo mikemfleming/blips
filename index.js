95f7380a4b9900dfa7699d233d1bfa95c9aef1bf
miek <flemingatx@gmail.com>
2018-04-21 19:42:19 -0500

    adds favicon route

--- index.js
+++ index.js
@@ -4,16 +4,15 @@ const express = require('express');
 const path = require('path');
 const log = require('./logger');
 const pino = require('pino-http')({ logger: log });
-const favicon = require('serve-favicon');
 
 const { PORT } = require('./config/main.config');
 
 const app = express();
 
 app.use(pino);
-app.use(favicon(path.join((__dirname, 'favicon.png'))));
-app.get('*/script.js', (req, res) => res.sendFile(path.join(__dirname, 'dist/bundle.js')));
 
+app.get('*/script.js', (req, res) => res.sendFile(path.join(__dirname, 'dist/bundle.js')));
+app.get('/favicon.png', (req, res) => res.sendFile(path.join(__dirname, 'favicon.png')));
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, './index.html')));
 
 app.listen(PORT, () => log.info(`listening on ${PORT}`));
